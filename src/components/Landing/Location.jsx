0471feda1481f48ebcc228317127a16f2b95ffdf
porperza <pikza_classic@hotmail.com>
2025-03-02 14:13:12 +0700

    complete merg

--- src/components/Landing/Location.jsx
+++ src/components/Landing/Location.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch , useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import "./style.css";
