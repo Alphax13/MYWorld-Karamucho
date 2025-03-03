8a06bc953316aad453103c5a89b854f46119aa08
Alphax13 <nuttaya.qx@gmail.com>
2025-03-03 22:17:38 +0700

    update

--- src/components/Landing/Event.jsx
+++ src/components/Landing/Event.jsx
@@ -16,12 +16,14 @@ const Event = () => {
       {/* Container ปุ่ม ใช้ absolute กำหนดตำแหน่ง + responsive */}
       <div className="absolute -10 top-[68%] right-[15%] md:right-[20%] lg:right-[20%] xl:right-[38%] flex flex-col items-center gap-40 md:gap-60 lg:gap-50 xl:gap-75 transform -translate-y-1/2">
         
-        {/* ปุ่ม คลิกเลย */}
-        <motion.button className="event-button bg-gradient-to-r from-[#004A5D] to-[#009BC3] text-white px-6 md:px-8 lg:px-10 py-3 md:py-4 lg:py-5 rounded-lg border border-[#28B7E1] shadow-md hover:from-[#003D4C] hover:to-[#008BB0] transition duration-300 text-lg md:text-xl lg:text-2xl"
+      {/* ปุ่ม คลิกเลย */}
+        <motion.button 
+        className="event-button bg-gradient-to-r from-[#004A5D] to-[#009BC3] text-white px-6 md:px-8 lg:px-10 py-3 md:py-4 lg:py-5 rounded-lg border border-[#28B7E1] shadow-md hover:from-[#003D4C] hover:to-[#008BB0] transition duration-300 text-lg md:text-xl lg:text-2xl"
         initial={{ y: 0 }}
         animate={{ y: [0, -10, 0] }}
-        transition={{ repeat: Infinity, duration: 1.5, ease: "easeInOut" }}>
-          คลิกเลย
+        transition={{ repeat: Infinity, duration: 1.5, ease: "easeInOut" }}
+        whileHover={{ scale: 1.1 }}>
+        คลิกเลย
         </motion.button>
 
         {/* ปุ่ม Add Line */}
