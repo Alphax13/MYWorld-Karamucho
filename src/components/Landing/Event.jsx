d0824109abb28894395b9b63e22e432fbee498bc
Alphax13 <nuttaya.qx@gmail.com>
2025-03-03 22:25:48 +0700

    update

--- src/components/Landing/Event.jsx
+++ src/components/Landing/Event.jsx
@@ -10,11 +10,11 @@ const Event = () => {
       <div className="w-full flex justify-center cursor-pointer">
         <img src="images/textevent.png" alt="Checkin Banner" className="w-[50%] md:w-[45%] lg:w-[40%] xl:w-[35%] 2xl:w-[30%] max-sm:w-[70%]" />
       </div>
-      <div className="absolute -10 top-[32%] lg:top-[40%] right-[10%] md:right-[23%] lg:right-[15%] xl:right-[38%] flex flex-col items-center transform -translate-y-1/2">
+      <div className="absolute -10 top-[32%] lg:top-[40%] right-[15%] md:right-[23%] lg:right-[15%] xl:right-[38%] flex flex-col items-center transform -translate-y-1/2">
         <h1 className="text-base lg:text-xl font-bold">ล่า COINS หมูกะทะ</h1>
       </div>
       {/* Container ปุ่ม ใช้ absolute กำหนดตำแหน่ง + responsive */}
-      <div className="absolute -10 top-[68%] right-[15%] md:right-[20%] lg:right-[20%] xl:right-[38%] flex flex-col items-center gap-40 md:gap-60 lg:gap-50 xl:gap-75 transform -translate-y-1/2">
+      <div className="absolute -10 top-[68%] right-[15%] md:right-[20%] lg:right-[20%] xl:right-[38%] flex flex-col items-center gap-35 md:gap-60 lg:gap-50 xl:gap-75 transform -translate-y-1/2">
         
       {/* ปุ่ม คลิกเลย */}
         <motion.button 
